3b3724111cef029fc05643093ceb6a9939c3ecd6
Nathan Fallet <contact@nathanfallet.me>
2019-01-21 21:46:54 +0100

    Updating copyright to 2019

--- CraftSearch/src/fr/zabricraft/craftsearch/utils/Translation.java
+++ CraftSearch/src/fr/zabricraft/craftsearch/utils/Translation.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2018 Groupe MINASTE
+ *  Copyright (C) 2019 Groupe MINASTE
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
